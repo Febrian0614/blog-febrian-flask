9c426ebece6a0702488ca25fa1445e8fe7cd5810
Febrian0614 <febrian.abirin123@gmail.com>
2023-09-07 11:47:01 +0700

    add brithdays.db,imports

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,13 @@
-# import aplikasi flask untuk dpakai di web kita
-from flask import Flask
+# import aplikasi flask,os,flask,jsonify,redirect,dan render_template untuk dipaki di web kita
+import os
+
+#impor 50SQL untuk akses database
+from cs50 import SQL
+#import flash untuk notifikasi pada web
+#import jsonify untuk memformat data
+#import redirect dan render_template untuk berpindah kehalaman web
+# import request dan session untuk mengakses data
+from flask import Flask,flash,jsonify,redirect,render_template,request,session
 
 #mengatur nama aplikasi
 app = Flask(__name__)
